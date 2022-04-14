6d4d37e92cc5a71a9232ab5080ae148311828c30
TanzilaKhann <tanzila9340@gmail.com>
2022-04-14 07:23:02 +0530

    initilized project

---
+++ src/main/java/com/OnlineClinicSystem/OnlineClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.OnlineClinicSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClinicSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OnlineClinicSystem/OnlineClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineClinicSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
